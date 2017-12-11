e3a6b9c83b5c1f96fa29153ec7ed079cd1618402
jessephillips <mr.jesse.phillips@gmail.com>
2017-12-11 15:47:58 -0500

    Separate admin and public actions for Projects.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
-  resources :projects
   devise_for :users
+  
+  namespace :admin do
+    resources :projects, except: [ :show ]
+  end
+  
   get :about,   controller: :pages
   get :faq,     controller: :pages
   get :home,    controller: :pages
   get :resume,  controller: :pages
-
+  
+  resources :projects, only: [ :index, :show ]
+  
   root 'pages#home'
 end
