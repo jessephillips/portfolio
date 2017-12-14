0811da76ef1e00295f986b02427fc46e61b8b1b1
Annihilous <benjaminjnorton@gmail.com>
2017-12-14 16:19:05 -0500

    Configure test suite.

--- config/application.rb
+++ config/application.rb
@@ -11,8 +11,15 @@ module Portfolio
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    config.generators do | g |
+      g.assets          false
+      g.helper          false
+      g.jbuilder        false
+      g.stylesheets     false
+      g.test_framework :rspec,
+        view_specs:     false,
+        request_specs:  false,
+        routing_specs:  false
+    end
   end
 end
