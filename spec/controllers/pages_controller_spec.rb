661254e60444c983185ef00e571e5c45019b5229
jessephillips <mr.jesse.phillips@gmail.com>
2018-01-22 11:30:13 -0500

    Adding some tests

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -15,4 +15,47 @@ RSpec.describe PagesController, type: :controller do
       expect(response).to render_template('home')
     end
   end
+  
+  describe 'GET #about' do
+    it 'returns http success' do
+      get :about
+
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'renders the about template' do
+      get :about
+
+      expect(response).to render_template('about')
+    end
+  end
+  
+  describe 'GET #resume' do
+    it 'returns http success' do
+      get :resume
+
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'renders the resume template' do
+      get :resume
+
+      expect(response).to render_template('resume')
+    end
+  end
+  
+  describe 'GET #faq' do
+    it 'returns http success' do
+      get :faq
+
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'renders the faq template' do
+      get :faq
+
+      expect(response).to render_template('faq')
+    end
+  end
+  
 end
